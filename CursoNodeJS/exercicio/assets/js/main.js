4d15a596eac313dbfaca492ba8299c844206c9d9
Elias Floriano <eliasflloriano@gmail.com>
2023-02-09 15:23:03 -0300

    chore: exercicio finalizado

--- CursoNodeJS/exercicio/assets/js/main.js
+++ CursoNodeJS/exercicio/assets/js/main.js
@@ -8,27 +8,60 @@ form.addEventListener('submit', function (e) {
     const peso = Number(inputPeso.value);
     const altura = Number(inputAltura.value);
 
-    if(!peso) {
+    if (!peso) {
         setResultado('Peso inválido', false);
         return;
     }
 
-    if(!altura) {
+    if (!altura) {
         setResultado('Altura inválida', false);
         return;
     }
 
-    console.log(peso, altura);
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+    setResultado(msg, true);
+
+    
 });
 
-function criaP () {
+
+
+function getNivelImc(imc){
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if(imc >= 39.90) return nivel[5];
+    if(imc >= 34.90) return nivel[4];
+    if(imc >= 29.90) return nivel[3];
+    if(imc >= 24.90) return nivel[2];
+    if(imc >= 18.50) return nivel[1];
+    if(imc < 18.50) return nivel[0];
+}
+
+function getImc(peso, altura){
+    const imc = (peso / altura**2);
+    return imc.toFixed(2);
+}
+
+function criaP() {
     const p = document.createElement('p');
     return p;
 }
 
-function setResultado (msg, isValid) {
+function setResultado(msg, isValid) {
     const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = '';
+    resultado.innerHTML = ''
+
     const p = criaP();
 
+    if(isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+
+    p.innerHTML = msg;
+    resultado.appendChild(p);
 }
