72836a52e451e27b5ee4593ce1f4e1ee0fac2222
Elias Floriano <eliasflloriano@gmail.com>
2023-02-10 17:17:54 -0300

    chore: aulas

---
+++ CursoNodeJS/Aula34/assets/js/index.js
@@ -0,0 +1,11 @@
+ 
+//  for(let i=0; i<=10; i++){
+//     const par = i % 2 === 0 ? 'par' : 'impar';
+//     console.log(`Numero ${i}, é ${par}`);
+//  };
+
+const frutas = ['Manga', 'Pera', 'Abacaxi'];
+
+for(let i = 0; i < frutas.length; i++){
+    console.log(`Ìndice ${i}`, frutas[i]);
+}
