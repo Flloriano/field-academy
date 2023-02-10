6f48f6b088a3ecd3a99a517af57f837699edcf91
Flloriano <eliasflloriano@gmail.com>
2023-02-10 13:32:47 -0300

    Chore: exercicio

--- api-nodejs/index.js
+++ api-nodejs/index.js
@@ -1,9 +1,20 @@
 const axios = require('axios').default;
-const cTable = require('console.table'); 
 
-const BuscarCepURL = `https://viacep.com.br/ws/15048-112/json/`;
-const data = axios.get(BuscarCepURL);
-console.log(module.exports);
+axios = `https://viacep.com.br/ws/15048-112/json/`
+.then(resposta => data = axios);
+
+console.log(data);
+
+
+//const cTable = require('console.table'); 
+
+//const BuscarCepURL = `https://viacep.com.br/ws/15048-112/json/`;
+//const data = axios.get(BuscarCepURL);
+
+
+// module.exports
+
+// console.log(module.exports);
 
 
 // const table = cTable.getTable([
