e0451f144b54928710ff85473b7869d55b8ef6b3
Elias Floriano <eliasflloriano@gmail.com>
2023-02-13 15:31:57 -0300

    chore: add aula exercicio

---
+++ CursoNodeJS/Aula40/exercicio.js
@@ -0,0 +1,16 @@
+// function max(x, y){
+
+    
+//     //1º
+//     // if(x > y) return x;
+//     // return y;
+
+//   //2º
+//   //  return x > y ? x : y;
+    
+
+// }
+
+//3º
+const max = (x, y) => x > y ? x : y;
+console.log(max(15, 10));
---
+++ CursoNodeJS/Aula40/exercicio2.js
@@ -0,0 +1,10 @@
+// function imagem(largura, altura){
+//     if(largura > altura) return true;
+//     return false;
+
+// }
+// console.log(imagem(80, 40));
+    
+
+const imagem = (largura, altura) => largura > altura ? true : false;
+console.log(imagem(10, 40));
