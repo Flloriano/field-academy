f022bec0f671d52702b5a8aaed0add300ae8b367
Flloriano <eliasflloriano@gmail.com>
2023-02-27 16:50:57 -0300

    feat: aulas

---
+++ ExercicioApi/exportar.js
@@ -0,0 +1,19 @@
+const { promises: fs } = require('fs');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+const csvWriter = createCsvWriter({
+    path: './forecast.csv',
+    header: ['nome', 'salario', 'estado', 'idade'].map((item) => ({ id: item, title: item}))
+})
+
+async function main() {
+    const file_data = await fs.readFile('pessoas.json');
+    const parsed_data = JSON.parse(file_data);
+
+    // try{
+    //     await csvWriter.writeRecords();
+    // } catch (error) {
+    //     console.log(error);
+    // }
+
+}
