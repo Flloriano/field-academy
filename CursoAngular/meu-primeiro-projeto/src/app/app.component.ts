5fdb9ed79241958f3f5aaaa7545144c0700fab46
Flloriano <eliasflloriano@gmail.com>
2023-02-28 17:42:11 -0300

    feat: secção 3 e 4

--- CursoAngular/meu-primeiro-projeto/src/app/app.component.ts
+++ CursoAngular/meu-primeiro-projeto/src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit} from '@angular/core';
+
+
+/*
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked
+*/
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template:`
+  
+    <app-title title="Olá Mundo"></app-title>
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent {
-  title = 'meu-primeiro-projeto';
+export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+
+  constructor() { 
+  }
+
+  ngOnInit(): void { }
+  ngDoCheck(): void { 
+    console.log("ngDoCheck");
+  }
+  ngAfterContentInit(): void { 
+    console.log("ngAfterContentInit");
+  }
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked");
+  }
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit");
+  }
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked");
+  }
+  
+  
+  
+  
+
 }
+//ng generate --help
+//ng g c title
